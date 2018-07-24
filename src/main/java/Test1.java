71fbbd9518838d274b1975d235327147a2c64844
xzh.xzh <xzh.xzh@antfin.com>
2018-07-24 14:08:56 +0800

    dz

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -6,6 +6,8 @@
 public class Test1 {
 
     public static void main(String[] args) {
-        System.out.println("TEST1");
+        System.out.println("test2");
+
+        System.out.println("dz  test2");
     }
 }
