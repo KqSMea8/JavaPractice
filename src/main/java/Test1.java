97e6d6b4b5d344829de7857c626863fdaf1f3619
xzh.xzh <xzh.xzh@antfin.com>
2018-07-24 14:01:04 +0800

    TEST1

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -3,12 +3,9 @@
  *
  * @author xzh  2018/7/24
  */
-public class Test {
+public class Test1 {
 
     public static void main(String[] args) {
-
-          System.out.println("dz");
-
-          System.out.println("2");
+        System.out.println("TEST1");
     }
 }
