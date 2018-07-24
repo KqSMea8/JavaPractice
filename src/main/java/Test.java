0b6eb15a9604dedae4548c92949977522d75c658
xzh.xzh <xzh.xzh@antfin.com>
2018-07-24 13:51:49 +0800

    main

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Alipay.com Inc Copyright (c) 2004-2017 All Rights Reserved.
+ *
+ * @author xzh  2018/7/24
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
