31327c70121f040522ed8cda79eeee3bd423d6fc
xzh.xzh <xzh.xzh@antfin.com>
2018-07-25 16:16:52 +0800

    master test5

---
+++ src/main/java/Test5.java
@@ -0,0 +1,11 @@
+/**
+ * Alipay.com Inc Copyright (c) 2004-2017 All Rights Reserved.
+ *
+ * @author xzh  2018/7/25
+ */
+public class Test5 {
+
+    public static void main(String[] args) {
+        System.out.println("master test5");
+    }
+}
